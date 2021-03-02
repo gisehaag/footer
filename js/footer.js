5f38e86c4fa4374645c7f712fd2fc334a737b4a3
Gise Haag <ing.giselahaag@gmail.com>
2021-03-02 10:21:42 +0100

    Add span tag on texts

--- js/footer.js
+++ js/footer.js
@@ -24,13 +24,13 @@ class Footer {
 		this.footerBox.innerHTML =
 			`<div class="footer-content">
 				<p>
-					<i><img width="12px" src="https://s.w.org/images/core/emoji/13.0.1/svg/270f.svg" alt="✏" /></i>
-					Developed with
-					<i><img width="12px" src="https://s.w.org/images/core/emoji/13.0.1/svg/2764.svg" alt="❤" /></i>
-					by
+					<i> <img width="12px" src="https://s.w.org/images/core/emoji/13.0.1/svg/270f.svg" alt="✏" /> </i>
+					<span>Developed with</span>
+					<i> <img width="12px" src="https://s.w.org/images/core/emoji/13.0.1/svg/2764.svg" alt="❤" /> </i>
+					<span>by</span>
 					<a href="https://gisehaag.com" target="_blank">GisehaaG</a>
-					<i><img width="12px" src="https://s.w.org/images/core/emoji/13.0.1/svg/2b50.svg" alt="⭐" /></i>
-					|
+					<i> <img width="12px" src="https://s.w.org/images/core/emoji/13.0.1/svg/2b50.svg" alt="⭐" /> </i>
+					// <span> | </span>
 					<i> ${year} </i>
 				</p>
 			</div>`
